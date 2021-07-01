e70b54ee108bc071ebc55c9c77a8438e478a17a3
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2021-07-01 03:06:14 -0400

    clean up ignored files

--- src/ts/commands/ping.ts
+++
@@ -1,7 +0,0 @@
-import type {Message, Client} from "discord.js";
-
-
-
-export default function ping(message: Message, client: Client): void {
-	message.channel.send(`pong -- latency ${Date.now() - message.createdTimestamp}ms, api latency ${Math.round(client.ws.ping)}ms`);
-}
