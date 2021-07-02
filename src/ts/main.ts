6c1320fc06a79caff0a45ab954e85dd0c2b9eee7
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2021-07-02 01:13:06 -0400

    style

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,14 +1,13 @@
 import * as Discord from "discord.js";
 import * as fs from "fs-extra";
 import Bot from "./Bot.js";
-import BitMath from "./BitMath.js";
 
 const client = new Discord.Client();
 
 
 const config = JSON.parse(fs.readFileSync("./config.json", {encoding: "utf8", flag: "r"}));
 
-var b = BitMath
+
 
 client.on("ready", () => { // move client into Bot class?
 	console.log("ready");
