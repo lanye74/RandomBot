fc24361e091c03903a8c51598950c00bf8f20774
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2021-07-14 16:44:08 -0400

    Rewrote console color command to not be dogshit

--- src/ts/util/console.ts
+++ src/ts/util/console.ts
@@ -1,75 +1,74 @@
 enum ConsoleColors {
-	Reset = "\x1b[0m",
-	Bold = "\x1b[1m",
-	Faint = "\x1b[2m",
-	Underscore = "\x1b[4m",
-	Blink = "\x1b[5m",
-	Reverse = "\x1b[7m",
-	Hidden = "\x1b[8m",
+	RESET = "\x1b[0m",
+	BOLD = "\x1b[1m",
+	FAINT = "\x1b[2m",
+	UNDERSCORE = "\x1b[4m",
+	BLINK = "\x1b[5m",
+	REVERSE = "\x1b[7m",
+	HIDDEN = "\x1b[8m",
 
-	Black = "\x1b[30m",
-	Red = "\x1b[31m",
-	Green = "\x1b[32m",
-	Yellow = "\x1b[33m",
-	Blue = "\x1b[34m",
-	Magenta = "\x1b[35m",
-	Cyan = "\x1b[36m",
-	White = "\x1b[37m",
 
-	BgBlack = "\x1b[40m",
-	BgRed = "\x1b[41m",
-	BgGreen = "\x1b[42m",
-	BgYellow = "\x1b[43m",
-	BgBlue = "\x1b[44m",
-	BgMagenta = "\x1b[45m",
-	BgCyan = "\x1b[46m",
-	BgWhite = "\x1b[47m"
+	BLACK = "\x1b[30m",
+	RED = "\x1b[31m",
+	GREEN = "\x1b[32m",
+	YELLOW = "\x1b[33m",
+	BLUE = "\x1b[34m",
+	MAGENTA = "\x1b[35m",
+	CYAN = "\x1b[36m",
+	WHITE = "\x1b[37m",
+
+
+	BGBLACK = "\x1b[40m",
+	BG_BLACK = "\x1b[40m",
+	BGRED = "\x1b[41m",
+	BG_RED = "\x1b[41m",
+	BGGREEN = "\x1b[42m",
+	BG_GREEN = "\x1b[42m",
+	BGYELLOW = "\x1b[43m",
+	BG_YELLOW = "\x1b[43m",
+	BGBLUE = "\x1b[44m",
+	BG_BLUE = "\x1b[44m",
+	BGMAGENTA = "\x1b[45m",
+	BG_MAGENTA = "\x1b[45m",
+	BGCYAN = "\x1b[46m",
+	BG_CYAN = "\x1b[46m",
+	BGWHITE = "\x1b[47m",
+	BG_WHITE = "\x1b[47m"
 };
 
 type ConsoleColor = keyof typeof ConsoleColors;
 
-
-
-// export default function f(color: string, text: string): string {
-	// return `${ConsoleColors[<ConsoleColor>color]}${text}${ConsoleColors.Reset}`;
-// }
+function CCify(key: string): string {
+	return ConsoleColors[<ConsoleColor>key.toUpperCase()];
+}
 
 
 
 export default function f(...args: string[]): string {
-	const inputtedFormats = args.filter((_val, index) => index % 2 === 0);
-	const inputtedStrings = args.filter((_val, index) => index % 2 === 1);
-	
+	const argumentPairs: string[][] = [];
 	let stringBuilder = "";
 
+	args.reduce((result, _value, index, array) => { // split the args into format/text pairs
+		if(index % 2 === 0) {
+			argumentPairs.push(array.slice(index, index + 2));
+		}
 
-	for(let i = 0; i < inputtedFormats.length; i++) {
-		const [currentFormat, currentText] = [inputtedFormats[i], inputtedStrings[i]];
+		return result;
+	}, []);
+
+
+
+	for(const currentPair of argumentPairs) {
+		const [formatString, text] = currentPair;
+		const formats = formatString.split("+").map(format => format.trim());
 		
-		let inbetweenFormats: string[] = [];
-		// this is so ugly
 
-		if(currentFormat.includes("+")) {
-			for(const inbetween of currentFormat.split("+")) {
-				inbetweenFormats.push(inbetween.trim()); // I hate this with every fiber in my being
-			}
+		for(const format of formats) {
+			stringBuilder += CCify(format); // convert to appropriate console color
 		}
 
-		if(inbetweenFormats.length) { // it does contain items
-			for(const format of inbetweenFormats) {
-				stringBuilder += ConsoleColors[<ConsoleColor>format];
-			}
-		} else {
-			stringBuilder += ConsoleColors[<ConsoleColor>currentFormat]; // this is awful, how to simplify?
-		}
-
-		// I hate this code, please rename everything soon
-
-		stringBuilder += currentText;
+		stringBuilder += text;
 	}
 
-
-	return (stringBuilder + ConsoleColors.Reset);
+	return stringBuilder + CCify("Reset");
 }
-
-// this is the second worst function I've ever had the displeasure of writing
