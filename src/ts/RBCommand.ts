0d335238dd1b7969c00d0332629c5bd9745cbdc3
LayneTheEpic <layne.ogden727@gmail.com>
2021-10-23 23:22:51 -0400

    convert ban command to class, rewrite a little bit

--- src/ts/RBCommand.ts
+++ src/ts/RBCommand.ts
@@ -1,6 +1,11 @@
+import Bot from "./Bot";
+
+
+
 export default class RBCommand {
 	static description: string = "Placeholder description";
 	static friendlyName: string = "Base Command";
+	protected static prefix = Bot.config.prefix;
 
 	static run(...args: any): any {
 		console.log("This is a command!");
