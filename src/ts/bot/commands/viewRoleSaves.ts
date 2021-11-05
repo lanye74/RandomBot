fb1a0d8d37a964dc525b4002607be7bfd6aa74ff
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2021-11-05 18:44:34 -0400

    im going to go fucking insane what is this ass code

--- src/ts/bot/commands/viewRoleSaves.ts
+++ src/ts/bot/commands/viewRoleSaves.ts
@@ -1,6 +1,6 @@
 import {createHash} from "crypto"; // compute hash and cache embeds later (only most recent)
 import FSManager from "../FSManager.js";
-import {MessageEmbed} from "discord.js";
+import {EmbedFieldData, MessageEmbed} from "discord.js";
 import RBCommand from "../RBCommand.js";
 
 import type {MessageCommand} from "../types.js";
@@ -16,22 +16,18 @@ type ParsedRoleSave = {
 
 
 
-class sha1 {
-	static hash = createHash("sha1");
+function toSHA1Hash(what: string): string {
+	return createHash("sha1").update(what).digest("base64");
+	// benchmarking led me to discover that sha-1 hex is fastest
+	// digest type makes little difference in the testing; however base64 is more efficient at encoding data
+	// leaderboard:
 
-	static toHash(what: string) {
-		return this.hash.update(what).digest("base64");
-		// benchmarking led me to discover that sha-1 hex is fastest
-		// digest type makes little difference in the testing; however base64 is more efficient at encoding data
-		// leaderboard:
-
-		// md5-hex x 72,128 ops/sec ±1.20% (88 runs sampled)
-		// md5-base64 x 72,478 ops/sec ±1.69% (90 runs sampled)
-		// sha1-hex x 139,350 ops/sec ±2.90% (89 runs sampled)
-		// sha1-base64 x 139,769 ops/sec ±3.25% (88 runs sampled)
-		// sha256-hex x 135,440 ops/sec ±3.87% (85 runs sampled)
-		// sha256-base64 x 133,716 ops/sec ±4.37% (89 runs sampled)
-	}
+	// md5-hex x 72,128 ops/sec ±1.20% (88 runs sampled)
+	// md5-base64 x 72,478 ops/sec ±1.69% (90 runs sampled)
+	// sha1-hex x 139,350 ops/sec ±2.90% (89 runs sampled)
+	// sha1-base64 x 139,769 ops/sec ±3.25% (88 runs sampled)
+	// sha256-hex x 135,440 ops/sec ±3.87% (85 runs sampled)
+	// sha256-base64 x 133,716 ops/sec ±4.37% (89 runs sampled)
 }
 
 
@@ -41,8 +37,8 @@ export default class viewRoleSaves extends RBCommand {
 	static friendlyName = "View Role Saves";
 	static usage = "viewRoleSaves {page | id} {page # | save id}";
 	static cachedServers: Map<string, [string, ParsedRoleSave[]]> = new Map();
-	static cachedRoleNames: Map<string, string> = new Map();
 	//                        id       hash    data
+	static cachedRoleNames: Map<string, string> = new Map();
 
 	static async run(command: MessageCommand) {
 		const {args, channel, guild} = command;
@@ -65,7 +61,7 @@ export default class viewRoleSaves extends RBCommand {
 		let parsedSaves: ParsedRoleSave[] = [];
 
 
-		const cachedHash = sha1.toHash(JSON.stringify(serverSaves));
+		const cachedHash = toSHA1Hash(JSON.stringify(serverSaves));
 
 		if(this.cachedServers.has(guild.id) && this.cachedServers.get(guild.id)![0] === cachedHash) {
 			parsedSaves = this.cachedServers.get(guild.id)![1];
@@ -76,24 +72,46 @@ export default class viewRoleSaves extends RBCommand {
 		}
 
 
+		if(parsedSaves.length === 0) {
+			embed.setDescription("This server has no saves.");
+			channel.send(embed);
+			return;
+		}
+
+
 
 		if(args.length === 0) {
 			const first10 = parsedSaves.slice(0, 10);
 
-			first10.forEach(async save => {
-				// ass
-				// cock
-				// balls
-				const role = (this.cachedRoleNames.has(save.roleID)) ? this.cachedRoleNames.get(save.roleID) : (async () => {const role = await guild.roles.fetch(save.roleID); this.cachedRoleNames.set(save.roleID, role!.name); return role});
-				// ass
-				// shit
-				// cum
-				// piss
-				// im working on this later
+			const fieldsQueue: Function[] = [];
 
-
-				embed.addField(`ID: ${save.saveID}`, "Role:");
+			first10.forEach(save => {
+				fieldsQueue.push(() => loadRoleField(save));
 			});
+
+			const fields = await Promise.all(fieldsQueue.map(field => field()))
+			embed.addFields(...fields);
+			channel.send(embed);
+
+			async function loadRoleField(save: ParsedRoleSave): Promise<any> {
+				return new Promise(async resolve => {
+					let role = viewRoleSaves.cachedRoleNames.get(save.roleID);
+					debugger
+
+					if(role === undefined) {
+						const discordRole = await guild.roles.fetch(save.roleID);
+						let role = discordRole!.name;
+
+						if(!discordRole) {
+							role = "Deleted Role";
+						}
+
+						viewRoleSaves.cachedRoleNames.set(save.roleID, role);
+					}
+
+					resolve(<EmbedFieldData>{name: `ID: ${save.saveID}`, value: `Role: "${role}"; ${save.members.length} members`, inline: false});
+				});
+			}
 		} else if(args[0] === "page") {
 			const pages = Math.ceil(parsedSaves.length / 10);
 			const pageRequested = parseInt(args[1]);
@@ -141,7 +159,7 @@ export default class viewRoleSaves extends RBCommand {
 			return 0;
 		});
 
-		const recomputedHash = sha1.toHash(JSON.stringify(out));
+		const recomputedHash = toSHA1Hash(JSON.stringify(out));
 		this.cachedServers.set(guildID, [recomputedHash, out]);
 
 		return out;
