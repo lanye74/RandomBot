ce8bcd426f65780ba0a106f7323cfaccb9d05119
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2022-07-31 21:25:06 -0400

    fuck you discord

--- src/ts/types/consts.ts
+++ src/ts/types/consts.ts
@@ -1,8 +1,8 @@
 import type {GatewayIntentsString} from "discord.js";
 
 export const RandomBotIntentPresets = {
-	GuildAll: <GatewayIntentsString[]>["Guilds", "GuildBans", "GuildEmojisAndStickers", "GuildIntegrations", "GuildInvites", "GuildMembers", "GuildMessageReactions", "GuildMessageTyping", "GuildMessages", "GuildPresences", "GuildVoiceStates", "GuildWebhooks"],
-	GuildBasic: <GatewayIntentsString[]>["Guilds", "GuildBans", "GuildMembers", "GuildMessages"],
-	DMs: <GatewayIntentsString[]>["DirectMessages", "DirectMessageReactions", "DirectMessageTyping"],
+	GuildAll: <GatewayIntentsString[]>["Guilds", "GuildBans", "GuildEmojisAndStickers", "GuildIntegrations", "GuildInvites", "GuildMembers", "GuildMessageReactions", "GuildMessageTyping", "GuildMessages", "GuildPresences", "GuildVoiceStates", "GuildWebhooks", "MessageContent"],
+	GuildBasic: <GatewayIntentsString[]>["Guilds", "GuildBans", "GuildMembers", "GuildMessages", "MessageContent"],
+	DMs: <GatewayIntentsString[]>["DirectMessages", "DirectMessageReactions", "DirectMessageTyping", "MessageContent"],
 	Reactions: <GatewayIntentsString[]>["DirectMessageReactions", "GuildMessageReactions"]
 } as const;
